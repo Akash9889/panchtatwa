449f9a43a986412a47119ff75ead1d53654cd629
Akash Gupta <akash.serene@gmail.com>
2022-05-14 18:19:29 +0530

    added google map link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -34,6 +34,17 @@ export default function Footer() {
               {" "}
               Phone: <span>+91-9810046385</span>
             </a>
+            <iframe
+              style={{ width: "90%", height: "200px", marginTop: "20px" }}
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.149223593961!2d77.35147901503338!3d28.505157782468224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce734aa340fa9%3A0x42cb9a4482d2e451!2sPanchtatwa-The%20Law%20of%20Creation%20(Guruji%20Manoj%20k%20Juyal)!5e0!3m2!1sen!2sin!4v1652532037044!5m2!1sen!2sin"
+              width="600"
+              title="Panchtatwa-The Law of Creation (Guruji Manoj k Juyal)"
+              height="450"
+              // style="border:0;"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
           <img className="flower" src={flowerDivider} alt="" />
           <div className="social-media-plugins-conatiner">
